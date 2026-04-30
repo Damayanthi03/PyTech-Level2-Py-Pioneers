2412df479117f006f08907697919ab8f772918db
Sunkari Damayanthi <sunkarinarayanarao104@gmail.com>
2026-04-30 21:14:55 +0530

    fix merge conflicts

--- Sales-Summary-Dashboard/main.py
+++ Sales-Summary-Dashboard/main.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
+
 from src.dashboard import sales_dashboard
 
-=======
-from src.dashboard import sales_dashboard
-
->>>>>>> 48bb3c2917ac7080f94d425528dea3b9c2e4dad0
 sales_dashboard()
